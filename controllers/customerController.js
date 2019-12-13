35125bbdda7ba14f03767914db2c679439027620
Vishal Patel <saralbhagat0@gmail.com>
2019-12-13 21:24:52 +0800

    Add Controllers customer, order, payment,product

---
+++ controllers/customerController.js
@@ -0,0 +1,87 @@
+const { Customer } = require('../models/Customer');
+const asyncMiddleware = require('../middlewares/async');
+
+module.exports = {
+    createOrUpdateCustomer: asyncMiddleware(async (req, res) => {
+        let errors = {};
+        const user = req.user._id;
+        const { name, phone } = req.body;
+        let customerFields = {};
+
+        customerFields.user = user;
+        if (name) customerFields.name = name;
+        if (phone) customerFields.phone = phone;
+
+        const customer = await Customer.findOne({ user });
+
+        if (customer) {
+            const updateCustomer = await Customer.findOneAndUpdate(
+                { user },
+                { $set: customerFields },
+                { new: true }
+            );
+            return res.json(updateCustomer);
+        }
+
+        const newCustomer = await new Customer(customerFields).save();
+
+        res.json(newCustomer);
+
+    }),
+    addCustomerAddress: asyncMiddleware(async (req, res) => {
+        const errors = {};
+        const { street, location, number } = req.body;
+        const newAddress = {
+            street,
+            location,
+            number
+        };
+
+        const customer = await Customer.findOne({ user: req.user._id });
+        if (!customer) {
+            errors.notFound = '"User" not found';
+            return res.status(400).json('"user" not found');
+        }
+        customer.address = [...customer.address, newAddress];
+        const updateCustomer = await customer.save();
+        res.json(updateCustomer);
+
+    }),
+    deleteCustomerAddress: asyncMiddleware(async (req, res) => {
+        const errors = {};
+        const user = req.user._id;
+        const addressId = req.value.params
+        const customer = await Customer.findOne({ user });
+        if (!customer) {
+            errors.notFound = "customer not found";
+            return res.status(404).json('"customer" not found');
+        }
+
+        customer.address = [...customer.address.filter(a => a._id.toString() !== addressId)];
+        await customer.save();
+        res.status(200).json(customer);
+    }),
+    getCustomers: asyncMiddleware(async (req, res) => {
+        //TODO GET CUSTOMERS
+    }),
+    getCustomer: asyncMiddleware(async (req, res) => {
+        const errors = {};
+        const customer = await Customer.findOne({ user: req.user._id });
+        if (!customer) {
+            errors.notFound = '"notFound" User not found';
+            return res.status(400).json('"notFound" User not found');
+        }
+        res.status(200).json(customer);
+    }),
+    deleteCustomer: asyncMiddleware(async (req, res) => {
+        const errors = {};
+        const _id = req.value.params;
+        const customer = await Customer.findByIdAndRemove(_id);
+        if (!customer) {
+            errors.notFound = 'Customer not found'
+            return res.status(400).json('"customer" not found')
+        }
+
+        res.status(200).json({ success: true });
+    })
+}
---
+++ controllers/paymentController.js
@@ -0,0 +1,172 @@
+const { Order } = require('../models/Order');
+const { Product } = require('../models/Product');
+const { Customer } = require('../models/Customer');
+const request = require("request");
+const JWT = require("jsonwebtoken");
+
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PAYPAL_API, JWT_SECRET_CART, JWT_SECRET, RETURN_URL, CANCEL_URL } = require("../config/keys");
+
+const verifyUserAuth = async bearerToken => {
+  if (!bearerToken) {
+    return null;
+  }
+  try {
+    if (typeof bearerToken !== 'undefined') {
+      const bearer = bearerToken.split(' ');
+      const token = bearer[1];
+      const decoded = await JWT.verify(token, JWT_SECRET);
+      return decoded;
+
+    }
+    else {
+      return null;
+    }
+
+  }
+  catch (err) {
+    return null;
+  }
+}
+module.exports = {
+  createPayment: function (req, res) {
+    // 2. Call /v1/payments/payment to set up the payment
+    const token = req.params.token;
+    const { items } = JWT.verify(token, JWT_SECRET_CART);
+    const cart = items.cart.map(item => {
+      return {
+        name: item.product.name,
+        price: item.product.discount && item.product.discount > 0
+          ? Number(
+            item.product.price -
+            item.product.discount * item.product.price * 0.01
+          ).toFixed(2).toString() : item.product.price.toString(),
+        currency: "USD",
+        quantity: item.quantity
+      }
+    })
+
+    request.post(PAYPAL_API + '/v1/payments/payment',
+      {
+        auth:
+        {
+          user: PAYPAL_CLIENT_ID,
+          pass: PAYPAL_CLIENT_SECRET
+        },
+        body:
+        {
+          intent: 'sale',
+          payer:
+          {
+            payment_method: 'paypal'
+          },
+          transactions: [
+            {
+              item_list: {
+                items: cart
+              },
+              amount:
+              {
+                total: items.totalAmount.toString(),
+                currency: 'USD'
+              }
+            }],
+          redirect_urls:
+          {
+            return_url: RETURN_URL,
+            cancel_url: CANCEL_URL
+          }
+        },
+        json: true
+      }, function (err, response) {
+        if (err) {
+          console.error(err);
+          return res.status(500).send(err);
+        }
+        // 3. Return the payment ID to the client
+
+        res.json(
+          {
+            id: response.body.id
+          });
+      });
+  },
+  executePayment: async function (req, res) {
+
+    const user = await verifyUserAuth(req.header('authorization'));
+    if (user === null) {
+      return res.status(401).send('Access denied. No token provided.');
+    }
+    const token = req.params.token;
+    const { items } = JWT.verify(token, JWT_SECRET_CART);
+
+    // Execute the payment:
+    // 1. Set up a URL to handle requests from the PayPal button.
+    // request.post('/execute-payment', function (req, res) {
+    // 2. Get the payment ID and the payer ID from the request body.
+
+    const { paymentID, payerID, shipAddress } = req.body;
+
+    // 3. Call /v1/payments/payment/PAY-XXX/execute to finalize the payment.
+    request.post(PAYPAL_API + '/v1/payments/payment/' + paymentID +
+      '/execute',
+      {
+        auth:
+        {
+          user: PAYPAL_CLIENT_ID,
+          pass: PAYPAL_CLIENT_SECRET
+        },
+        body:
+        {
+          payer_id: payerID,
+          transactions: [
+            {
+              amount:
+              {
+                total: items.totalAmount.toString(),
+                currency: 'USD'
+              }
+            }]
+        },
+        json: true
+      },
+      async function (err, response) {
+        if (err) {
+          console.error(err);
+          return res.status(500).send(err);
+        }
+        // 4. Return a success response to the client 
+        const customer = await Customer.findOne({ user: user._id }).select('_id');
+        if (!customer) {
+          errors.notFound = 'Customer not found';
+          return res.status(400).json(errors);
+        }
+        items.cart.map(async item => {
+          const stock = item.product.stock - item.quantity;
+          await Product.findByIdAndUpdate(item.product._id, { stock: stock }, { new: true });
+        })
+
+        const order = new Order({
+          customer: customer._id,
+          items,
+          paymentId: paymentID,
+          shipAddress
+        });
+
+        await order.save();
+
+        const payment = {
+          paid: true,
+          cancelled: false,
+          payerID: response.body.payer.payer_info.payer_id,
+          paymentID: response.body.id,
+        };
+        res.json(
+          {
+            status: 'success',
+            payment
+          });
+      });
+
+  }
+}
+
